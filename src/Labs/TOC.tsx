982a059ddff491191ea8de28ee4daf5699790a42
angj28 <106568112+angj28@users.noreply.github.com>
2025-02-17 02:51:40 -0500

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,7 +2,7 @@ import Nav from "react-bootstrap/Nav";
 import { useLocation } from "react-router";
 
 export default function TOC() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
   return (
     <Nav variant="pills">
@@ -10,13 +10,19 @@ export default function TOC() {
         <Nav.Link href="#/Labs">Labs</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab1" active={pathname.includes("Lab1")}>Lab 1</Nav.Link>
+        <Nav.Link href="#/Labs/Lab1" active={pathname.includes("Lab1")}>
+          Lab 1
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab2" active={pathname.includes("Lab2")}>Lab 2</Nav.Link>
+        <Nav.Link href="#/Labs/Lab2" active={pathname.includes("Lab2")}>
+          Lab 2
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab3" active={pathname.includes("Lab3")}>Lab 3</Nav.Link>
+        <Nav.Link href="#/Labs/Lab3" active={pathname.includes("Lab3")}>
+          Lab 3
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
@@ -37,7 +43,7 @@ export default function TOC() {
           Assignment 2 Github
         </Nav.Link>
       </Nav.Item>
-            <Nav.Item>
+      <Nav.Item>
         <Nav.Link
           id="wd-github"
           href="https://github.com/angj28/kambaz-react-web-app/tree/a3"

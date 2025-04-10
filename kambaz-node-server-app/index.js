478926f3638127aa96ad7a0d8c93f94844c0ebfe
angj28 <106568112+angj28@users.noreply.github.com>
2025-04-09 23:49:26 -0400

    help

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -33,7 +33,7 @@ const sessionOptions = {
 if (process.env.NODE_ENV === "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie.sameSite = "none";
-  // sessionOptions.cookie.secure = true;
+  sessionOptions.cookie.secure = true;
   if (process.env.NODE_SERVER_DOMAIN) {
     sessionOptions.cookie.domain = process.env.NODE_SERVER_DOMAIN;
   }
